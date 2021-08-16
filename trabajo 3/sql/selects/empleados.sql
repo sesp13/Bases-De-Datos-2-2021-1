2c6edbe531471f1a2ebddabfe0c6d1491ef357bb
Sesp13 <sespinosarte99@gmail.com>
2021-08-16 00:51:46 -0500

    agregar queries de trabajo

--- trabajo 3/sql/selects/empleados.sql
+++ trabajo 3/sql/selects/empleados.sql
@@ -1,3 +1,33 @@
 SELECT * FROM empleado;
 
-SELECT DEREF(v.miprod) FROM empleado e, TABLE(ventas) v;
+--Con arreglo 
+SELECT * FROM empleado e, TABLE(e.ventas) v;
+
+--Obtener ventas por ciudad
+SELECT 
+e.cc,
+v.miprod.cod as codProducto, 
+v.nro_unidades as nroUnidades 
+FROM empleado e, TABLE(e.ventas) v
+WHERE miciu = (SELECT REF(c) FROM ciudad c WHERE cod = %s)
+
+--Obtener ventas por empleado
+SELECT 
+e.cc,
+v.miprod.cod as codProducto, 
+v.nro_unidades as nroUnidades,
+v.miprod.precio_unitario as precioProducto,
+(v.miprod.precio_unitario) * v.nro_unidades as total
+FROM empleado e, TABLE(e.ventas) v;
+
+
+--Get empleado con mayor venta
+SELECT * FROM (
+  SELECT
+  e.cc as cc,
+  SUM((v.miprod.precio_unitario) * v.nro_unidades) AS total
+  FROM empleado e, TABLE(e.ventas) v
+  GROUP BY e.cc
+  ORDER BY total DESC
+) WHERE rownum <= 1;
+
--- trabajo 3/sql/selects/ventas.sql
+++ trabajo 3/sql/selects/ventas.sql
@@ -1,2 +1,8 @@
 -- Ventas por ciudad
 SELECT * FROM empleado e, TABLE(e.ventas)
+
+--Get total ventas ciudad
+SELECT 
+SUM((v.miprod.precio_unitario) * v.nro_unidades) AS total
+FROM empleado e, TABLE(e.ventas) v
+WHERE e.miciu = (SELECT REF(c) FROM ciudad c WHERE cod = %s)
