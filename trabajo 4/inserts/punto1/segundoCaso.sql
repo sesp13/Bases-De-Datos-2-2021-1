e75c425dedb80d44cf76f3c47345de1c6a1d8313
Sesp13 <sespinosarte99@gmail.com>
2021-09-02 23:22:13 -0500

    reestructurar proyecto

---
+++ trabajo 4/inserts/punto1/segundoCaso.sql
@@ -0,0 +1,50 @@
+--30.000 clientes
+--60.000 facturas
+--80.000 detalles
+
+--- Crear clientes
+BEGIN
+FOR i IN 1..30000 LOOP
+  INSERT INTO cliente
+  VALUES
+  (
+    i, 
+    DBMS_RANDOM.STRING('L',TRUNC(DBMS_RANDOM.VALUE(1,101))),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000))
+  );
+END LOOP;
+commit;
+END;
+/
+
+--Crear facturas
+BEGIN
+FOR i IN 1..60000 LOOP
+  INSERT INTO factura
+  VALUES
+  (
+    i, 
+    TRUNC(SYSDATE + DBMS_RANDOM.value(0,366)),
+    (SELECT * FROM(SELECT cedula FROM cliente SAMPLE (0.7)) WHERE ROWNUM = 1)
+  );
+END LOOP;
+commit;
+END;
+/
+
+--Crear detalles
+BEGIN
+FOR i IN 1..80000 LOOP
+  INSERT INTO detalle
+  VALUES
+  (
+    i, 
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    (SELECT * FROM(SELECT cecliente FROM factura SAMPLE (0.99)) WHERE ROWNUM = 1)
+  );
+END LOOP;
+commit;
+END;
+/
