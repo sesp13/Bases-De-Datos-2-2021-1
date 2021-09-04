6cbde9967372f44cd111bb0edf0fb25fc2a83617
Sesp13 <sespinosarte99@gmail.com>
2021-09-03 21:35:29 -0500

    agregar ultimo caso punto 1b

---
+++ trabajo 4/inserts/punto1/caso4.sql
@@ -0,0 +1,51 @@
+
+--200.000 clientes
+--400.000 facturas
+--600.000 detalles
+
+--- Crear clientes
+BEGIN
+FOR i IN 1..200000 LOOP
+  INSERT INTO cliente
+  VALUES
+  (
+    i, 
+    DBMS_RANDOM.STRING('L',TRUNC(DBMS_RANDOM.VALUE(1,101))),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000))
+  );
+END LOOP;
+commit;
+END;
+/
+
+--Crear facturas
+BEGIN
+FOR i IN 1..400000 LOOP
+  INSERT INTO factura
+  VALUES
+  (
+    i, 
+    TRUNC(SYSDATE + DBMS_RANDOM.value(0,366)),
+    (SELECT * FROM(SELECT cedula FROM cliente SAMPLE (0.01)) WHERE ROWNUM = 1)
+  );
+END LOOP;
+commit;
+END;
+/
+
+--Crear detalles
+BEGIN
+FOR i IN 1..600000 LOOP
+  INSERT INTO detalle
+  VALUES
+  (
+    i, 
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    TRUNC(DBMS_RANDOM.value(1,100000000000000000000)),
+    (SELECT * FROM(SELECT cecliente FROM factura SAMPLE (0.01)) WHERE ROWNUM = 1)
+  );
+END LOOP;
+commit;
+END;
+/
